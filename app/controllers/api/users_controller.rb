5fb7c6300a64f53888bb64c3eab281c842229b54
Tyler Myracle <tyler.myracle@gmail.com>
2024-02-01 18:15:15 -0600

    update redirects

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,16 @@
 class Api::UsersController < ApplicationController
+  before_action :authenticate_user!
   def client_user
     if current_user
       render json: current_user
     else
+      render json: { error: "Not logged in" }, status: :unauthorized
+    end
+  end
+
+  private
+  def authenticate_user!
+    unless user_signed_in?
       render json: { error: "Not logged in" }, status: :unauthorized
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :set_csrf_cookie
-
-  def after_sign_in_path_for(user)
-    ENV["CLIENT_URL"]
-  end
+  before_action :redirect_if_signed_in
 
   private
   
   def set_csrf_cookie
     cookies["CSRF-TOKEN"] = form_authenticity_token
+  end
+
+  def redirect_if_signed_in
+    return unless request.format.html?
+
+    if user_signed_in?
+      redirect_to ENV["CLIENT_URL"], allow_other_host: true
+    end
   end
 end
