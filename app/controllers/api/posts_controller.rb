822d479187f4c0488965cb35446fd5289c286948
Tyler Myracle <tyler.myracle@gmail.com>
2024-02-13 22:58:03 -0600

    add reactions to posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -9,7 +9,8 @@ class Api::PostsController < ApplicationController
     render json: posts.as_json(include: {
       user: { only: [:id, :name, :github_image, :avatar_url] },
       group: { only: [:id, :name, :url_slug] },
-      comments: { include: { user: { only: [:id, :name, :avatar_url] } } }
+      comments: { include: { user: { only: [:id, :name, :avatar_url] } } },
+      reactions: { only: [:id, :post_id, :emoji_code, :emoji_text, :user_id] }
     })
   end
   
