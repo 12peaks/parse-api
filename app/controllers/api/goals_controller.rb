3d3bc3b557720e337727e4845c78465e625f2435
Tyler Myracle <tyler.myracle@gmail.com>
2024-03-03 13:31:41 -0600

    formatting

--- app/controllers/api/goals_controller.rb
+++ app/controllers/api/goals_controller.rb
@@ -7,10 +7,12 @@ class Api::GoalsController < ApplicationController
     user = current_user || api_user
     goals = user.current_team.goals
     render json: goals.as_json(include: [
-      { user: { only: [:id, :name], methods: [:avatar_image_url] } },
-      { goal_updates: { include: { user: { only: [:id, :name], methods: [:avatar_image_url] } }, order: 'created_at ASC' } },
-      { goal_collaborators: { include: { user: { only: [:id, :name], methods: [:avatar_image_url] } } } }
-    ])
+                                 { user: { only: %i[id name], methods: [:avatar_image_url] } },
+                                 { goal_updates: { include: { user: { only: %i[id name], methods: [:avatar_image_url] } },
+                                                   order: 'created_at ASC' } },
+                                 { goal_collaborators: { include: { user: { only: %i[id name],
+                                                                            methods: [:avatar_image_url] } } } }
+                               ])
   end
 
   def show
@@ -18,12 +20,14 @@ class Api::GoalsController < ApplicationController
     goal = user.current_team.goals.find_by(id: params[:id])
     if goal
       render json: goal.as_json(include: [
-        { user: { only: [:id, :name], methods: [:avatar_image_url] } },
-        { goal_updates: { include: { user: { only: [:id, :name], methods: [:avatar_image_url] } }, order: 'created_at ASC' } },
-        { goal_collaborators: { include: { user: { only: [:id, :name], methods: [:avatar_image_url] } } } }
-      ])
+                                  { user: { only: %i[id name], methods: [:avatar_image_url] } },
+                                  { goal_updates: { include: { user: { only: %i[id name], methods: [:avatar_image_url] } },
+                                                    order: 'created_at ASC' } },
+                                  { goal_collaborators: { include: { user: { only: %i[id name],
+                                                                             methods: [:avatar_image_url] } } } }
+                                ])
     else
-      render json: { error: "Goal not found in your current team" }, status: :not_found
+      render json: { error: 'Goal not found in your current team' }, status: :not_found
     end
   end
 
@@ -33,10 +37,11 @@ class Api::GoalsController < ApplicationController
     goal.user = user
     if goal.save
       render json: goal.as_json(include: [
-        { user: { only: [:id, :name], methods: [:avatar_image_url] } },
-        { goal_updates: { only: [:id, :note, :value, :status, :created_at, :updated_at] } },
-        { goal_collaborators: { include: { user: { only: [:id, :name], methods: [:avatar_image_url] } } } }
-      ])
+                                  { user: { only: %i[id name], methods: [:avatar_image_url] } },
+                                  { goal_updates: { only: %i[id note value status created_at updated_at] } },
+                                  { goal_collaborators: { include: { user: { only: %i[id name],
+                                                                             methods: [:avatar_image_url] } } } }
+                                ])
     else
       puts goal.errors.full_messages
       render json: { error: goal.errors.full_messages }, status: :bad_request
@@ -49,15 +54,16 @@ class Api::GoalsController < ApplicationController
     if goal
       if goal.update(goal_params)
         render json: goal.as_json(include: [
-          { user: { only: [:id, :name], methods: [:avatar_image_url] } },
-          { goal_updates: { only: [:id, :note, :value, :status, :created_at, :updated_at] } },
-          { goal_collaborators: { include: { user: { only: [:id, :name], methods: [:avatar_image_url] } } } }
-        ])
+                                    { user: { only: %i[id name], methods: [:avatar_image_url] } },
+                                    { goal_updates: { only: %i[id note value status created_at updated_at] } },
+                                    { goal_collaborators: { include: { user: { only: %i[id name],
+                                                                               methods: [:avatar_image_url] } } } }
+                                  ])
       else
         render json: { error: goal.errors.full_messages }, status: :bad_request
       end
     else
-      render json: { error: "Goal not found in your current team" }, status: :not_found
+      render json: { error: 'Goal not found in your current team' }, status: :not_found
     end
   end
 
@@ -66,15 +72,16 @@ class Api::GoalsController < ApplicationController
     goal = user.current_team.goals.find_by(id: params[:id])
     if goal
       goal.destroy
-      render json: { success: true, message: "Goal deleted" }
+      render json: { success: true, message: 'Goal deleted' }
     else
-      render json: { error: "Goal not found in your current team" }, status: :not_found
+      render json: { error: 'Goal not found in your current team' }, status: :not_found
     end
   end
-  
+
   private
 
   def goal_params
-    params.require(:goal).permit(:name, :description, :user, :team, :format, :start_date, :end_date, :initial_value, :target_value, goal_collaborators: [])
+    params.require(:goal).permit(:name, :description, :user, :team, :format, :start_date, :end_date, :initial_value,
+                                 :target_value, goal_collaborators: [])
   end
 end
